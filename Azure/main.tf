bb255faacb18d1efa1904036d1eae4a8e672afa4
Tim Krebs <timkrebs9@gmail.com>
2025-03-18 08:15:13 +0100

    Add Terraform configuration files for AWS and Azure with .gitignore

---
+++ Azure/main.tf
@@ -0,0 +1,28 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  cloud {
+    organization = "ACME-Corp_Demo"
+
+    workspaces {
+      name = "learn-terraform"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
